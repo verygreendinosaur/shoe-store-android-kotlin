f13e197b7fa8cd925111b6cb759481671383d9da
Chris Poole <chris.st.poole@gmail.com>
2020-12-24 21:34:25 -0500

    Partially implement through step 7

---
+++ ShoeStore/app/src/main/java/com/verygreendinosaur/android/shoestore/LoginFragment.kt
@@ -0,0 +1,35 @@
+package com.verygreendinosaur.android.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.verygreendinosaur.android.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_login, container, false)
+
+        setupButtons()
+        return binding.root
+    }
+
+    private fun setupButtons() {
+        binding.signInButton.setOnClickListener { navigateToWelcome(it) }
+        binding.createAccountButton.setOnClickListener { navigateToWelcome(it) }
+    }
+
+    private fun navigateToWelcome(v: View) {
+        v.findNavController().navigate(R.id.action_loginFragment_to_welcomeFragment)
+    }
+
+}
---
+++ ShoeStore/app/src/main/java/com/verygreendinosaur/android/shoestore/WelcomeFragment.kt
@@ -0,0 +1,34 @@
+package com.verygreendinosaur.android.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.verygreendinosaur.android.shoestore.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_welcome, container, false)
+
+        setupButtons()
+        return binding.root
+    }
+
+    private fun setupButtons() {
+        binding.button2.setOnClickListener{ navigateToInstructions(it) }
+    }
+
+    private fun navigateToInstructions(v: View) {
+        // v.findNavController().navigate(R.id.action_welcomeFragment_to_instructionsFragment)
+    }
+
+}
